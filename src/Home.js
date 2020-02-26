6b478d95310a7a2a871fef648ac9096ca26359b3
aisukdl <51831489+aisukdl@users.noreply.github.com>
2020-02-26 23:56:43 +0700

    background updated

--- src/Home.js
+++ src/Home.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './Home.css';
 import {Link} from 'react-router-dom';
 import logo from './icon.png'
+import bg from './bg.png'
 
 
 
 function Home() {
   return (
     <div className="home-container" style={{fontFamily:"Source Sans Pro"}}>
-        <img src={logo} />
+      <div className="white">
+        <img src={logo} alt=""/>
         <p>ผลงานจากการวิจัย</p>
         <p>โดย</p>
         <p>นิสิตชั้นปีที่​ 4 สาขาวิชาภาษาฝรั่งเศส​เพื่อการสื่อสาร​</p>
@@ -18,6 +20,7 @@ function Home() {
         <p>59021089 นางสาวอดิ​ชญา​ ศุขประเสริฐ</p>
         <p>ผู้ผลิตแอพพลิเคชั่น aisukdl.</p>
         <Link to="/category"><center><div className="start" style={{color:"#fff"}}><h2>เริ่มต้นใช้งาน</h2></div></center></Link>
+      </div>
     </div>
   );
 }
